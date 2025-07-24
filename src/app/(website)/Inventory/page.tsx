983f604c855ea61ef821dbe41f3850bbd0f617bf
ILhankhondaker <ilhankhondaker@gmail.com>
2025-07-24 11:30:10 +0600

    add page header in inventory

--- src/app/(website)/Inventory/page.tsx
+++ src/app/(website)/Inventory/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ProductsPage from './_components/ProductsPage'
+import BannerSection from '@/components/homeHeaders/BannerSection'
 
 const page = () => {
     return (
         <div>
+                   <BannerSection image='/password.jpg' title='Browse Our All Products' description="High-quality products for every need – Indoor & Outdoor High-quality products for every"/>
             <ProductsPage />
         </div>
     )
