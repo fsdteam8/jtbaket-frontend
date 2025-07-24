c007b4a68f1b988f638596761cdadd5839989986
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-07-24 18:02:38 +0600

    added

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "@/app/globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { Toaster } from "sonner";
 
 const WebsiteLayout = ({ children }: { children: React.ReactNode }) => {
   return <div>
     <Navbar />
+    <Toaster/>
     {children}
     <Footer />
   </div>;
